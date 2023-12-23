7210991ee59b6e741eb07c4ee369aece56fb54e6
Tiphannie Margret <147834099+Tiphannie@users.noreply.github.com>
2023-12-23 15:40:46 +0100

    Update Calculator.java

--- src/funwithcalculators3/Calculator.java
+++ src/funwithcalculators3/Calculator.java
@@ -4,8 +4,7 @@ package funwithcalculators3;
  * The main class of a simple calculator. Create one of these and you'll
  * get the calculator on screen.
  * 
- * @author David J. Barnes and Michael Kolling
- * @version 2008.03.30
+ *
  */
 public class Calculator
 {
