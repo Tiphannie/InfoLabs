3e29c70e4b9c335ee37e9a10312ac049cdd17559
Tiphannie Margret <147834099+Tiphannie@users.noreply.github.com>
2023-12-23 15:38:08 +0100

    Update Calculator.java

--- src/calculator2/Calculator.java
+++ src/calculator2/Calculator.java
@@ -3,9 +3,7 @@ package calculator2;
 /**
  * The main class of a simple calculator. Create one of these and you'll
  * get the calculator on screen.
- * 
- * @author David J. Barnes and Michael Kolling
- * @version 2008.03.30
+ *
  */
 public class Calculator
 {
