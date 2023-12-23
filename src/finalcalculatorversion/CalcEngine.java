565e9f8c3802bcd5f6096d3d594910f85bfa6e39
Tiphannie Margret <147834099+Tiphannie@users.noreply.github.com>
2023-12-23 15:39:26 +0100

    Update CalcEngine.java

--- src/finalcalculatorversion/CalcEngine.java
+++ src/finalcalculatorversion/CalcEngine.java
@@ -3,8 +3,7 @@ package finalcalculatorversion;
 	/**
 	 * The main part of the calculator doing the calculations.
 	 * 
-	 * @author  David J. Barnes and Michael Kolling 
-	 * @version 2008.03.30
+	 *
 	 */
 	public class CalcEngine
 	{
